2bcb6fc07675709dd7fd8e0b46ce03926ac4619e
Gabriele Vassallo <gabri.vassallo01@gmail.com>
2016-03-02 15:15:12 +0100

    Add install.sh script

--- install.sh
+++ install.sh
@@ -2,14 +2,16 @@
 
 set -x 
 
-echo "Installing vim in $HOME directory" 
-
-ln -s $PWD/vim ~/.vim 
+echo "Intalling vim configuration"  
 
 echo "# Installing Vundle"
-git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git $PWD/vim/bundle/Vundle.vim
 
 echo "# Installing Vim plugins using Vundle"
 vim +PluginInstall +qall
+
+echo "Installing vim in $HOME directory" 
+
+ln -s $PWD/vim ~/.vim 
 
 
