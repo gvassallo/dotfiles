ceb8911e7bec5e117f6151824d5bc3bc53789368
Gabriele Vassallo <gabri.vassallo01@gmail.com>
2016-03-02 15:26:08 +0100

    Add Vundle.vim as submodule

--- install.sh
+++ install.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
+echo "Updating submodules" 
+
+git submodule update --init
 
 set -x 
 
-echo "Intalling vim configuration"  
-
-echo "# Installing Vundle"
-git clone https://github.com/gmarik/Vundle.vim.git $PWD/vim/bundle/Vundle.vim
-
-echo "# Installing Vim plugins using Vundle"
+echo "Installing Vim plugins using Vundle"
 vim +PluginInstall +qall
 
-echo "Installing vim in $HOME directory" 
-
+echo "Creating symlinks"
 ln -s $PWD/vim ~/.vim 
 
 
