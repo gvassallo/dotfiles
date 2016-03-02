69c8a2c36b5261707c76c43606b3b52128601242
Gabriele Vassallo <gabri.vassallo01@gmail.com>
2016-03-02 16:09:41 +0100

    Add Tmux configuration

--- install.sh
+++ install.sh
@@ -10,5 +10,6 @@ vim +PluginInstall +qall
 
 echo "Creating symlinks"
 ln -s $PWD/vim ~/.vim 
-
+ln -s $PWD/tmux ~/.tmux 
+ln -s $PWD/tmux.conf ~/.tmux.conf 
 
---
+++ tmux/README.md
@@ -0,0 +1,10 @@
+# Tmux Configuration 
+
+This is the personal Tmux configuration of Gabriele Vassallo. 
+<br> 
+<br> 
+Prefix is mapped to `Ctrl-A`. 
+
+
+
+
